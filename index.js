5e8f16b8c3118f2edfce0c7d4247ff784169bff6
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-06 20:53:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (C) 2017 Glayzzle (BSD3 License)
+ * @authors https://github.com/glayzzle/php-reflection/graphs/contributors
+ * @url http://glayzzle.com
+ */
+'use strict';
+
+module.exports = require('./src/graph');
