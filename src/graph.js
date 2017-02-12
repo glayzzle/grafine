1938cd4e37a8523c3274a283e7b1e49b63c0f6b1
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-12 22:55:31 +0100

    https://github.com/glayzzle/grafine/issues/3 start to use shard classes (wip)

--- src/graph.js
+++ src/graph.js
@@ -6,87 +6,122 @@
 'use strict';
 
 var point = require('./point');
+var shard = require('./shard');
+var index = require('./index');
 
 /**
  * Initialize a storage
  */
-var graph = function() {
-    this.points = [];
-    this.index = {};
-    this.length = 0;
-    this.gaps = 0;
+var graph = function(hash) {
+    this.hash = hash;
+    this.uuid = 0;
+    this.shards = [];
+    this.indexes = [];
+};
+
+/**
+ * Generate a uuid (autoincrement)
+ */
+graph.prototype.uuid = function() {
+    return ++this.uuid;
+};
+
+/**
+ * Retrieves a shard from the specified uuid
+ */
+graph.prototype.shard = function(uuid) {
+    var id = uuid % this.hash;
+    if (!this.shards[id]) {
+        this.shards[id] = this.createShard(id);
+    }
+    return this.shards[id];
+};
+
+/**
+ * Shard factory (lazy loading helper)
+ */
+graph.prototype.createShard = function(id) {
+    return new shard(this, id);
+};
+
+/**
+ * Retrieves an index shard from the specified key
+ */
+graph.prototype.index = function(key) {
+    var id = 0;
+    var size = key.length;
+    // if too long truncate in order to maintain a stable speed
+    if (size > this.hash) size = this.hash;
+    // parsing each letter into the string
+    for(var i = 0; i < size; i++) {
+        id = (id + key.charCodeAt(i)) % this.hash;
+    }
+    // create the index if not ready
+    if (!this.indexes[id]) {
+        this.indexes[id] = this.createIndex(id);
+    }
+    return this.indexes[id];
+};
+
+/**
+ * Index factory (lazy loading helper)
+ */
+graph.prototype.createIndex = function(id) {
+    return new index(this, id);
 };
 
 /**
  * Export all nodes as a plain object
  */
 graph.prototype.export = function() {
-    this.reindex();
-    var nodes = [];
-    for(var i = 0; i < this.points.length; i++) {
-        nodes.push(this.points[i].export());
+    var shards = [], indexes = [];
+    for(var i = 0; i < this.shards.length; i++) {
+        shards.push(this.shards[i] ? this.shards[i].export() : null);
     }
-    return nodes;
+    for(var i = 0; i < this.indexes.length; i++) {
+        indexes.push(this.indexes[i] ? this.indexes[i].export() : null);
+    }
+    return {
+        hash: this.hash,
+        uuid: this.uuid,
+        shards: shards,
+        indexes: indexes
+    };
 };
 
 /**
  * Importing all nodes
  */
-graph.prototype.import = function(points) {
+graph.prototype.import = function(data) {
+
     // initialize the context
-    this.gaps = 0;
-    this.length = points.length;
-    this.points = [];
-    this.index = {};
-    // create points
-    for(var i = 0, size = this.length; i < size; i++) {
-        var node = this.factory(points[i]);
-        this.points.push(node);
-        // indexing
-        var indexes = points[i]._i;
-        for(var j = 0; j < indexes.length; j++) {
-            var index = indexes[j];
-            if (!(index[0] in this.index)) {
-                this.index[index[0]] = {};
-                this.index[index[0]][index[1]] = [];
-            } else if (!(index[1] in this.index[index[0]])) {
-                this.index[index[0]][index[1]] = [];
-            }
-            this.index[index[0]][index[1]].push(node);
-        }
-    }
-    // link points
-    for(var i = 0, size = this.length; i < size; i++) {
-        var props = points[i]._p;
-        for(var k in props) {
-            var relations = props[k];
-            if (typeof relations === 'number') {
-                this.points[i].properties[k] = this.points[relations];
-                if (!(k in this.points[relations].related)) {
-                    this.points[relations].related[k] = [];
-                }
-                this.points[relations].related[k].push(this.points[i]);
-            } else {
-                this.points[i].properties[k] = [];
-                for(var j = 0; j < relations.length; j++) {
-                    var target = this.points[relations[j]];
-                    this.points[i].properties[k].push(target);
-                    if (!(k in this.points[relations[j]].related)) {
-                        this.points[relations[j]].related[k] = [];
-                    }
-                    this.points[relations[j]].related[k].push(this.points[i]);
-                }
+    this.hash = data.hash;
+    this.uuid = data.uuid;
+    this.shards = [];
+    this.indexes = [];
+
+    // import shards
+    if (data.shards && data.shards.length > 0) {
+        for(var i = 0, size = data.shards.length; i < size; i++) {
+            if (data.shards[i]) {
+                this.shards[i] = new shard(this, i);
+                this.shards[i].import(data.shards[i]);
             }
         }
     }
 
+    // import indexes
+    if (data.indexes && data.indexes.length > 0) {
+        for(var i = 0, size = data.indexes.length; i < size; i++) {
+            if (data.indexes[i]) {
+                this.indexes[i] = new index(this, i);
+                this.indexes[i].import(data.indexes[i]);
+            }
+        }
+    }
+
+    // job is done
     return this;
-};
-/**
- * Factory function
- */
-graph.prototype.factory = function(object) {
-    return new point(this);
 };
 
 /**
@@ -96,36 +131,12 @@ graph.prototype.create = function(result) {
     if (!result) {
         result = new point(this);
     }
-    this.points.push(result);
-    this.length ++;
+    // attach it to a shard
+    var uuid = this.uuid();
+    this.shard(uuid).push(uuid, result);
     return result;
 };
-/**
- * Cleanup gaps
- */
-graph.prototype.reindex = function() {
-    if (this.gaps === 0) return this;
-    var index = {};
-    this.points = this.points.filter(function(item) {
-      if (item != undefined) {
-          for(var i = 0; i < item.indexes.length; i++) {
-              var key = item.indexes[i];
-              if (!(key[0] in index)) {
-                  index[key[0]] = {};
-              }
-              if (!(key[1] in index[key[0]])) {
-                  index[key[0]][key[1]] = [];
-              }
-              index[key[0]][key[1]].push(item);
-          };
-          return true;
-      }
-      return false;
-    });
-    this.index = index;
-    this.gaps = 0;
-    return this;
-};
+
 /**
  * Proceed to a multicriteria search
  */
