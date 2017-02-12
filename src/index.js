6b65ed32f1a21503dd2396fd0ed411e7d6c1a363
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-12 23:32:45 +0100

    https://github.com/glayzzle/grafine/issues/3 fix index methods

--- src/index.js
+++ src/index.js
@@ -13,53 +13,73 @@ var index = function(db, id) {
     this.id = id;
     this.index = {};
     this.length = 0;
+    this.changed = false;
 };
 
 /**
- * Proceed to a multicriteria search
+ * Exports the index
  */
-index.prototype.search = function(criteria) {
-    var results = [];
-    // preparing results
-    for(var k in criteria) {
-        if (k in this.index) {
-            var hashes = this.index[k];
-            var check = criteria[k];
-            if (typeof check === 'function') {
-                for(var i in hashes) {
-                    if (check(i) === true) {
-                        results.push(hashes[i]);
-                    }
-                }
+index.prototype.export = function() {
+    this.changed = false;
+    return [
+        this.length,
+        this.index
+    ];
+};
+
+/**
+ * Imports the index
+ */
+index.prototype.import = function(data) {
+    this.length = data[0];
+    this.index = data[1];
+    this.changed = false;
+};
+
+/**
+ * Indexing the specified value
+ */
+index.prototype.add = function(key, value, point) {
+    if (!(key in this.index)) {
+        this.index[key] = {};
+    }
+    if (!(value in this.index[key])) {
+        this.index[key][value] = [];
+    }
+    if (point.uuid) point = point.uuid;
+    if (this.index[key][value].indexOf(point) === -1) {
+        this.changed = true;
+        this.index[key][value].push(point);
+        this.length ++;
+    }
+    return this;
+};
+
+/**
+ * Removes an entry from index
+ */
+index.prototype.remove = function(key, value, point) {
+    if (key in this.index && value in this.index[key]) {
+        if (point.uuid) point = point.uuid;
+        var index = this.index[key][value].indexOf(point);
+        if (index !== -1) {
+            this.changed = true;
+            if (this.index[key][value].length === 1) {
+                delete this.index[key][value];
             } else {
-                if (check in hashes) {
-                    results.push(hashes[check]);
-                }
+                this.index[key][value].splice(index, 1);
             }
-        } else return []; // nobody matches
-    }
-    if (results.length === 1) {
-        return results[0].filter(function(item) {
-          return item != undefined;
-        });
-    }
-    // intersect results (quick & dirty / may improve)
-    var data = [];
-    for(var s = 0; s < results.length; s++) {
-        var set = results[s];
-        for(var i = 0, size = set.length; i < size; i++) {
-            var miss = false;
-            var item = set[i];
-            if (!item) continue;
-            // scan other sets to check if exists
-            for(var j = 0; j < results.length; j++) {
-                if (j !== s && results[j].indexOf(item) === -1) {
-                    miss = true;
-                    break;
-                }
-            }
-            if (!miss) data.push(item);
         }
     }
-    return data;
+    return this;
+};
+
+/**
+ * Retrieves an index values (list of points)
+ */
+index.prototype.search = function(key, value) {
+    if (key in this.index && value in this.index[key]) {
+        return this.index[key][value];
+    }
+    return [];
 };
