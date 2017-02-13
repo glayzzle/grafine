fb2238e1255f353f616ca4a2053b56045a3164d3
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-13 22:08:41 +0100

    fix benchmark with new framework

--- bin/bench.js
+++ bin/bench.js
@@ -23,7 +23,7 @@ var elapsed_time = function(note, counter){
 
 // writing tests
 var last;
-var size = process.argv.length === 3 ? process.argv[2] : 1000;
+var size = process.argv.length === 3 ? process.argv[2] : 40000;
 for(var i = 0; i < size; i++) {
     var node = db.create();
     node.index('name', (i % 54321).toString(36));
@@ -41,30 +41,32 @@ elapsed_time('Create ' + size +  ' items', size);
 
 // deleting items
 var records = 10000;
+var idx = 0;
 for(var x = 0; x < 2; x++) {
-  for(var i = 0; i < records; i++) {
-      db.points[i * (size / records)].delete();
-  }
-  elapsed_time(x + '. Deleted ' + records +  ' items', records);
-  for(var i = 0; i < records; i++) {
-      var node = db.create();
-      node.index('name', i % 30);
-      node.index('type', i % 60);
-      if (i % 5 === 0) {
-          if (last) {
-              node.set('foo', last);
-              node.set('bar', last);
-              node.add('baz', last);
-          }
-      }
-      if (i % 10 === 0) last = node;
-  }
-  // check memory liberation
-  elapsed_time(x + '. Create ' + records +  ' items', records);
-
-  // rebuild index
-  db.reindex();
-  elapsed_time(x + '. Reindex ' + db.points.length +  ' items', db.length);
+    for(var i = 0; i < records; i++) {
+        var record = db.get(++idx);
+        if (record) {
+            record.delete();
+        } else {
+            console.log('Not found', idx);
+        }
+    }
+    elapsed_time(x + '. Deleted ' + records +  ' items', records);
+    for(var i = 0; i < records; i++) {
+        var node = db.create();
+        node.index('name', i % 30);
+        node.index('type', i % 60);
+        if (i % 5 === 0) {
+            if (last) {
+                node.set('foo', last);
+                node.set('bar', last);
+                node.add('baz', last);
+            }
+        }
+        if (i % 10 === 0) last = node;
+    }
+    // check memory liberation
+    elapsed_time(x + '. Create ' + records +  ' items', records);
 }
 
 // searching tests
@@ -79,9 +81,9 @@ for(var i = 0; i < search; i++) {
 }
 elapsed_time('Found ' + records +  ' items', records);
 var data = db.export();
-elapsed_time('Export ' + data.length +  ' items', data.length);
+elapsed_time('Export ' + db.size() +  ' items', db.size());
 db.import(data);
-elapsed_time('Import ' + db.length +  ' items', db.length);
+elapsed_time('Import ' + db.size() +  ' items', db.size());
 // searching tests
 search = 10;
 records = 0;
