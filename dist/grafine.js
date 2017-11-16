4a0394e9abe7b1246c5450ca2a07b2781fb00eb9
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-11-16 18:53:22 +0100

    release build

--- dist/grafine.js
+++ dist/grafine.js
@@ -1,4 +1,4 @@
-/*! grafine - BSD3 License - 2017-02-19 */
+/*! grafine - BSD3 License - 2017-11-16 */
 
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
@@ -43,6 +43,20 @@ module.exports = function(grafine) {
     Graph.prototype.uuid = function() {
         return ++this._nextId;
     };
+
+    /**
+     * Retrieves a list of shards
+     */
+    Graph.prototype.shards = function() {
+        return this._shards;
+    };
+
+    /**
+     * Retrieves a list of indexes
+     */
+    Graph.prototype.indexes = function() {
+        return this._indexes;
+    }
 
     /**
      * Retrieves a shard from the specified uuid
